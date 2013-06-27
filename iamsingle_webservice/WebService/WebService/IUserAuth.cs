b65d11151612c8c567557cc9fb30686d9e445bf3
unknown <tyrelltle@gmail.com>
2013-06-26 22:05:34 -0700

    add WCF webservice.   currently implemented register and logon

---
+++ iamsingle_webservice/WebService/WebService/IUserAuth.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService.WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUserAuth" in both code and config file together.
+    [ServiceContract]
+    public interface IUserAuth
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "register", RequestFormat=WebMessageFormat.Json,ResponseFormat=WebMessageFormat.Json, Method = "PUT")]
+        registerReturn register(UserInfo user);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "logon", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, Method = "POST")]
+        registerReturn logon(UserInfo user);
+    }
+
+    [DataContract]
+    public class registerReturn
+    {
+        private int mUid;
+        [DataMember]
+        public int userid { 
+            get{return mUid;}
+            set{mUid=value;}
+        }
+    }
+
+
+    [DataContract]
+    public class UserInfo
+    {
+        private string mEmail;
+        private string mPassword;
+        // Apply the DataMemberAttribute to the property.
+        [DataMember]
+        public string email
+        {
+            get { return mEmail; }
+            set { mEmail = value; }
+        }
+        [DataMember]
+        public string password
+        {
+            get { return mPassword; }
+            set { mPassword = value; }
+        }
+    }
+}
---
+++ iamsingle_webservice/WebService/WebService/UserAuth.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebService.WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserAuth" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select UserAuth.svc or UserAuth.svc.cs at the Solution Explorer and start debugging.
+    public class UserAuth : IUserAuth
+    {
+        public registerReturn register(UserInfo user)
+        {
+            DataClassesDataContext context = new DataClassesDataContext();
+
+            if(user==null||user.email==null)
+                //invalid input
+                return new registerReturn { userid = -1 };
+
+            Boolean has=((from u in context.Users
+                            where u.email == user.email
+                            select u).Count())>0;
+            if (has)
+                //user already exists
+                return new registerReturn { userid = -1 };
+
+            User newuser = new User() {  email=user.email, password=user.password};
+            context.Users.InsertOnSubmit(newuser);
+            context.SubmitChanges();
+            return new registerReturn() { userid = newuser.userid };
+
+
+        }
+
+
+
+
+        public registerReturn logon(UserInfo user)
+        {
+            DataClassesDataContext context = new DataClassesDataContext();
+
+            if (user == null || user.email == null)
+                //invalid input
+                return new registerReturn { userid = -1 };
+
+            var usr = (from u in context.Users
+                            where u.email == user.email
+                            select u);
+            if (usr==null||usr.Count()==0)
+                //user not exist
+                return new registerReturn { userid = -1 };
+
+            return new registerReturn() { userid = usr.Single().userid };
+
+
+        }
+    }
+}
