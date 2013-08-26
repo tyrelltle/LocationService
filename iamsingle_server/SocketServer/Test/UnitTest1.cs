16bb38a68a0c6c3416ebd32f264459b2f42df39d
tyrelltle <tyrelltle@gmail.com>
2013-08-25 22:13:33 -0700

    netsdk chat

--- iamsingle_server/SocketServer/Test/UnitTest1.cs
+++ iamsingle_server/SocketServer/Test/UnitTest1.cs
@@ -2,23 +2,47 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SocketServer;
 using SocketServer.ProxyHandlers;
-using SocketServer.Param;
+
 using System.Net.Sockets;
+using SocketServer.Utils;
+using System.Text;
 namespace Test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            string a = "qwesdasddsadsad1_0.11123_2.123124_3.123941_0.11123_2";
-            for (int i = 0; i < 15; i++)
-                a += "qwesdasddsadsad1_0.11123_2.123124_3.123941_0.11123_2";
-                
-            int w=System.Text.Encoding.UTF8.GetBytes(a).GetLength(0);
-           
-           
+        struct structure {
+            public int a ;
+        
         }
+        structure func()
+        {
+            structure a;
+            a.a = 101;
+            return a;
+        }
+        [TestMethod]
+        public void test()
+        {
+
+            structure a = func();
+            Assert.IsTrue(a.a == 101);
+
+        
+        }
+
+/*
+        [TestMethod]
+        public void TCPReceiverColleciton_TesT()
+        {
+
+            TCPRecieverCollection coll = TCPRecieverCollection.instance();
+            coll.add(0, new NetworkStream(new Socket(new SocketInformation())));
+            Assert.IsTrue(coll.getByUid(0)!=null);
+            Assert.IsTrue(coll.getByUid(1) == null);
+            coll.removeByUid(0);
+            Assert.IsTrue(coll.getByUid(0) == null);
+        }
+        */
     }
 }
