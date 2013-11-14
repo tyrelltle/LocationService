a56dda7ae7eb5016665e29d0a417e4bc435c81aa
tyrelltle <tyrelltle@gmail.com>
2013-11-14 01:03:57 -0800

    adding frieds web service

---
+++ iamsingle_webservice/WebService/WebService/FriendService.svc.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FriendService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select FriendService.svc or FriendService.svc.cs at the Solution Explorer and start debugging.
+    public class FriendService : IFriendService
+    {
+
+        public  const string FRIEND_CONNECTED="Friend Successfully Connected";//database has both (uid,fid) and (fid,uid) pair
+        public  const string PENDING_REQUEST = "Friend Connection Pending To Be Validated";//database has only (uid,fid)
+
+        public void DoWork()
+        {
+        }
+
+        public LinkedList<FriendData> getfriends(int uid)
+        {
+            LinkedList<FriendData> ret=new LinkedList<FriendData>();
+            DataClassesDataContext context=new DataClassesDataContext();
+            var baselist = (from f in context.Friends select new {a=f.uid,b=f.fuid } ).Union(
+                            from f in context.Friends select new {a=f.fuid,b=f.uid });
+
+            var finalist = from i in baselist
+                           join u in context.Users
+                           on i.a equals u.userid
+                           where i.b == uid
+                           select u;
+            foreach (var a in finalist)
+                ret.AddFirst(new FriendData { Name=a.username,Uid=a.userid });
+          return ret;
+          
+        }
+
+        public ReturnStatus addFriendById(int uid, int touid) {
+            LinkedList<FriendData> ret = new LinkedList<FriendData>();
+            
+            DataClassesDataContext context = new DataClassesDataContext();
+
+            Boolean exist = this.recordexists(context, uid, touid);
+            if (!exist) {
+                Friend newf = new Friend { uid = uid, fuid = touid };
+                context.Friends.InsertOnSubmit(newf);
+                context.SubmitChanges();
+            
+            }
+
+
+            return this.getReturn(context, uid, touid);
+
+
+        }
+
+
+        public ReturnStatus addFriendByEmail(int uid, string toemail)
+        {
+            LinkedList<FriendData> ret = new LinkedList<FriendData>();
+
+            DataClassesDataContext context = new DataClassesDataContext();
+            int touid = this.getuid(context, toemail);
+            Boolean exists = this.recordexists(context, uid, touid);
+            if (!exists)
+            {
+                Friend newf = new Friend { uid = uid, fuid = touid };
+                context.Friends.InsertOnSubmit(newf);
+                context.SubmitChanges();
+
+            }
+
+            return this.getReturn(context, uid, touid);
+
+
+        }
+
+        private int getuid(DataClassesDataContext c, string email)
+        {
+            int uid = (from i in c.Users
+                       where i.email == email
+                       select i.userid).First();
+            return uid;
+        }
+        private Boolean recordexists(DataClassesDataContext c, int uid, int touid)
+        {
+            return  c.Friends.Where(a => a.uid == uid && a.fuid == touid).Any();        
+        }
+
+        private ReturnStatus getReturn(DataClassesDataContext context,int uid, int touid) 
+        {
+            //now lets find out if your new friend already added u as friend
+            var tmp = (from i in context.Friends
+                       where i.fuid == uid && i.uid == touid
+                       select i);
+            switch (tmp.Count())
+            {
+                case 0:  //your new friend has not added u as friend yet 
+                    return new ReturnStatus { msg = PENDING_REQUEST, statuscode = 0 };
+                    break;
+
+                default: return new ReturnStatus { msg = FRIEND_CONNECTED, statuscode = 1 };
+
+
+            }
+        }
+    }
+}
---
+++ iamsingle_webservice/WebService/WebService/IFriendService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFriendService" in both code and config file together.
+    [ServiceContract]
+    public interface IFriendService
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "getfriends?uid={uid}", ResponseFormat = WebMessageFormat.Json, Method = "GET")]
+        LinkedList<FriendData> getfriends(int uid);
+        [OperationContract]
+        [WebInvoke(UriTemplate = "addFriendById?myuid={uid}&touid={touid}", ResponseFormat = WebMessageFormat.Json, Method = "PUT")]
+        ReturnStatus addFriendById(int uid, int touid);
+        [OperationContract]
+        [WebInvoke(UriTemplate = "addFriendByEmail?myuid={uid}&toemail={toemail}", ResponseFormat = WebMessageFormat.Json, Method = "PUT")]
+        ReturnStatus addFriendByEmail(int uid, string toemail);
+
+    }
+
+    [DataContract]
+    public class FriendData
+    {
+        private string mName;
+        private int mUid;
+        // Apply the DataMemberAttribute to the property.
+        [DataMember]
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+        [DataMember]
+        public int Uid
+        {
+            get { return mUid; }
+            set { mUid = value; }
+        }
+    }
+
+    [DataContract]
+    public class ReturnStatus
+    {
+        private int mStatusCode;//1 for success 0 for fail
+        private string  mmsg;
+        // Apply the DataMemberAttribute to the property.
+        [DataMember]
+        public int statuscode
+        {
+            get { return mStatusCode; }
+            set { mStatusCode = value; }
+        }
+        [DataMember]
+        public string msg
+        {
+            get { return mmsg; }
+            set { mmsg = value; }
+        }
+    }
+}
